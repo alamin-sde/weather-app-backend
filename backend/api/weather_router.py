2306018072873367556ea4adece94e9a887dc415
alamin-sde <ai.alamin.sde@gmail.com>
2025-11-03 02:19:09 +0530

    openAi and openWeather api integrated

--- backend/api/weather_router.py
+++ backend/api/weather_router.py
@@ -1,10 +1,20 @@
 from fastapi import APIRouter, Depends
 from core.database import getDB
 from sqlalchemy.ext.asyncio import AsyncSession
-from services.weather_service import fetWeatherByCity
+from services import weather_service
+from datetime import datetime
 router = APIRouter(prefix='/weather',tags=["Weather"])
 @router.get('/city')
-async def fetchWeatherByCity(db: AsyncSession = Depends(getDB)):
-    return await fetWeatherByCity('Kolkata',db)
+async def fetch_Weather_By_City(city: str):
+    return await weather_service.fetch_Weather_By_City(city)
+@router.get('/summary/city')
+async def getSummary(city:str):
+    return await weather_service.getWeatherSummary(city)
+    
+
+@router.get('/city/by_date')
+async def get_weather_by_date(city:str,target_date:datetime):
+    return await weather_service.get_weather_by_date(city,target_date)
+    
     
     
--- backend/services/weather_service.py
+++ backend/services/weather_service.py
@@ -3,76 +3,80 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import HTTPException
 from httpx import AsyncClient
 from dotenv import load_dotenv
+from services.ml_service import get_llm_summary,get_five_days_weather_summary
+from datetime import datetime
+import json
+import re
 load_dotenv()
 WEATHER_GEO = "http://api.openweathermap.org/geo/1.0/direct"
-ONECALL = "https://api.openweathermap.org/data/2.5/weather"
+ONECALL = "https://api.openweathermap.org/data/2.5"
+
 OPENWEATHER_API_KEY=os.getenv('OPENWEATHER_API_KEY')
-async def fetWeatherByCity(city:str,db:AsyncSession):
+def clean_llm_output(raw_output: str):
+    cleaned = re.sub(r"^```json|```$", "", raw_output.strip(), flags=re.MULTILINE).strip()
+    return json.loads(cleaned)
+
+async def fetch_Weather_By_City(city:str):
     try:
         async with AsyncClient(timeout=20) as client:
             
-            # geo_url = f"{WEATHER_GEO}?q={city}&limit=1&appid={OPENWEATHER_API_KEY}"
-            # g = await client.get(geo_url)
-            # g.raise_for_status()
-            # geo = g.json()
-            # if not geo:
-            #     raise ValueError(f"City not found: {city}")
-            # lat=geo[0]["lat"]
-            # lon=geo[0]["lon"]
-            # onecall_url = f"{ONECALL}?lat={lat}&lon={lon}&appid={OPENWEATHER_API_KEY}"
-            # r = await client.get(onecall_url)
-            # r.raise_for_status()
-            # data = r.json()
-            data={
-            "coord": {
-                "lon": 88.3577,
-                "lat": 22.5414
-            },
-            "weather": [
-                {
-                "id": 721,
-                "main": "Haze",
-                "description": "haze",
-                "icon": "50d"
-                }
-            ],
-            "base": "stations",
-            "main": {
-                "temp": 300.11,
-                "feels_like": 302.27,
-                "temp_min": 300.11,
-                "temp_max": 300.11,
-                "pressure": 1008,
-                "humidity": 74,
-                "sea_level": 1008,
-                "grnd_level": 1007
-            },
-            "visibility": 4000,
-            "wind": {
-                "speed": 5.14,
-                "deg": 330
-            },
-            "clouds": {
-                "all": 75
-            },
-            "dt": 1762067519,
-            "sys": {
-                "type": 1,
-                "id": 9114,
-                "country": "IN",
-                "sunrise": 1762042283,
-                "sunset": 1762082926
-            },
-            "timezone": 19800,
-            "id": 1277155,
-            "name": "Bara Bazar",
-            "cod": 200
-            }
-                    
+            geo_url = f"{WEATHER_GEO}?q={city}&limit=1&appid={OPENWEATHER_API_KEY}"
+            g = await client.get(geo_url)
+            g.raise_for_status()
+            geo = g.json()
+            if not geo:
+                raise ValueError(f"City not found: {city}")
+            lat=geo[0]["lat"]
+            lon=geo[0]["lon"]
+            onecall_url = f"{ONECALL}/weather?lat={lat}&lon={lon}&appid={OPENWEATHER_API_KEY}"
+            r = await client.get(onecall_url)
+            r.raise_for_status()
+            data = r.json()    
             return data
         
     except Exception as e:
         raise HTTPException(e)
+async def getWeatherSummary(city:str):
+    try:
+        raw = await fetch_Weather_By_City(city) 
+        data = await get_llm_summary(raw)
+        return data.strip()
+    except Exception as e:
+        raise HTTPException(e)
+
+async def get_weather_by_date(city:str,target_date:datetime):
+    try:
+        async with AsyncClient(timeout=20) as client:
+            geo_url = f"{WEATHER_GEO}?q={city}&limit=1&appid={OPENWEATHER_API_KEY}"
+            g = await client.get(geo_url)
+            g.raise_for_status()
+            geo = g.json()
+            if not geo:
+                raise ValueError(f"City not found: {city}")
+            lat=geo[0]["lat"]
+            lon=geo[0]["lon"]
+            forecast_url = f"{ONECALL}/forecast?lat={lat}&lon={lon}&appid={OPENWEATHER_API_KEY}"
+            r = await client.get(forecast_url)
+            r.raise_for_status()
+            forecast_data = r.json()
+            selected_day=[]
+            for day in   forecast_data.get("list"):
+                day_date = datetime.fromtimestamp(day['dt']).date()
+                if day_date==target_date.date():
+                    selected_day.append(day)
+            
+            if  len(selected_day)==0:
+                return {"error":f"No forecast available for {target_date}"} 
+            structuredData = await get_five_days_weather_summary(selected_day,target_date)  
+            return clean_llm_output(structuredData)
+            # return selected_day
+            
+    except Exception as e:
+        raise HTTPException(e)
+
+    
+    
+    
         
         
     
